5e8fd8ff3a52cab9302d47bff047802f862fd2da
shahedkh <alkhalailah.shahed@gmail.com>
2022-06-26 13:07:50 +0300

    register user

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,24 +1,52 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { db } from "../firebase.config";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 
 function SignUp() {
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
-    name :"",
+    name: "",
     email: "",
     password: "",
   });
-  const { email, password , name } = formData;
+  const { email, password, name } = formData;
 
   const navigate = useNavigate();
 
   const onChange = (e) => {
-    setFormData((prevState) =>({
-      ...prevState , 
-      [e.target.id]: e.target.value
-    }))
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      const user = userCredential.user;
+
+      updateProfile(auth.currentUser, {
+        displayName: name
+      })
+      navigate('/')
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -28,15 +56,15 @@ function SignUp() {
           <p className="pageHeader">Welcome Back!</p>
         </header>
 
-        <form>
-        <input
+        <form onSubmit={onSubmit}>
+          <input
             type="text"
             className="nameInput"
             placeholder=" Name"
             id="name"
             value={name}
             onChange={onChange}
-          /> 
+          />
 
           <input
             type="email"
@@ -68,14 +96,14 @@ function SignUp() {
             Forgot Password
           </Link>
 
-          <div className='signUpBar'>
-            <p className='signUpText'>Sign Un</p>
-            <button className='signUpButton'>
-              <ArrowRightIcon fill='#ffffff' width='34px' height='34px' />
+          <div className="signUpBar">
+            <p className="signUpText">Sign Un</p>
+            <button className="signUpButton">
+              <ArrowRightIcon fill="#ffffff" width="34px" height="34px" />
             </button>
           </div>
         </form>
-        <Link to='/sign-up' className='registerLink'>
+        <Link to="/sign-up" className="registerLink">
           Sign In Instead
         </Link>
       </div>
