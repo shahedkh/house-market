df1143f7e8f070871f4aec98b68b647cc5975385
shahedkh <alkhalailah.shahed@gmail.com>
2022-06-27 12:19:36 +0300

    add auth status

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,14 @@
+import { Navigate, Outlet } from "react-router-dom";
+import {useAuthStatus} from '../hooks/useAuthStatus'
+
+function PrivateRoute() {
+  const {loggedIn , checkingStatus} = useAuthStatus()
+
+  if(checkingStatus) { 
+    return <h3>loading...</h3>
+  }
+
+  return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
+}
+
+export default PrivateRoute;
