946d05a53a3349795aa4d4e42742f4324049bba4
shahedkh <alkhalailah.shahed@gmail.com>
2022-06-27 22:34:17 +0300

    fetch listings

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -13,11 +13,12 @@ import { db } from "../firebase.config";
 import { toast } from "react-toastify";
 import Spinner from "../components/Spinner";
 import ListingItem from "../components/ListingItem";
-import { FirebaseError } from "firebase/app";
 
 function Category() {
   const [listings, setListings] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [lastFetchedListing, setLastFetchedListing] = useState(null)
+
 
   const params = useParams();
 
@@ -33,12 +34,56 @@ function Category() {
           limit(10)
         )
         const querySnap = await getDocs(q)
-      } catch (error) {}
+
+        let listing = []
+        querySnap.forEach((doc) => {
+            return listing.push({
+                id: doc.id,
+                data: doc.data()
+            })
+        })
+        
+        setListings(listings)
+        setLoading(false)
+      } catch (error) {
+        toast.error('Could not fetch listings')
+      }
     };
     fetchListings();
-  });
+  },[params.categoryName]);
 
-  const onFetchMoreListings = () => {};
+  const onFetchMoreListings = async() => {
+    try {
+        const listingsRef = collection(db, 'listings')
+  
+        const q = query(
+          listingsRef,
+          where('type', '==', params.categoryName),
+          orderBy('timestamp', 'desc'),
+          startAfter(lastFetchedListing),
+          limit(10)
+        )
+  
+        const querySnap = await getDocs(q)
+  
+        const lastVisible = querySnap.docs[querySnap.docs.length - 1]
+        setLastFetchedListing(lastVisible)
+  
+        const listings = []
+  
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          })
+        })
+  
+        setListings((prevState) => [...prevState, ...listings])
+        setLoading(false)
+      } catch (error) {
+        toast.error('Could not fetch listings')
+      }
+  };
 
   return (
     <div className="category">
