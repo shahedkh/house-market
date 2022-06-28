099ae63e84e3c96ac3fccbc823fec1498de1bdae
shahedkh <alkhalailah.shahed@gmail.com>
2022-06-28 13:06:29 +0300

    uploading image

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -14,89 +14,199 @@ import { v4 as uuidv4 } from "uuid";
 import Spinner from "../components/Spinner";
 
 function CreateListing() {
-  const [geolocationEnabled, setGeolocationEnabled] = useState(true);
-  const [loading, setLoading] = useState(false);
-  const [formData, setFormData] = useState({
-    type: "rent",
-    name: "",
-    bedrooms: 1,
-    bathrooms: 1,
-    parking: false,
-    furnished: false,
-    address: "",
-    offer: false,
-    regularPrice: 0,
-    discountedPrice: 0,
-    images: {},
-    latitude: 0,
-    longitude: 0,
-  });
-
-  const {
-    type,
-    name,
-    bedrooms,
-    bathrooms,
-    parking,
-    furnished,
-    address,
-    offer,
-    regularPrice,
-    discountedPrice,
-    images,
-    latitude,
-    longitude,
-  } = formData;
-
-  const auth = getAuth();
-  const navigate = useNavigate();
-  const isMounted = useRef(true);
-
-  useEffect(() => {
-    if (isMounted) {
-      onAuthStateChanged(auth, (user) => {
-        if (user) {
-          setFormData({ ...formData, userRef: user.uid });
-        } else {
-          navigate("/sign-in");
+    const [geolocationEnabled, setGeolocationEnabled] = useState(true)
+    const [loading, setLoading] = useState(false)
+    const [formData, setFormData] = useState({
+      type: 'rent',
+      name: '',
+      bedrooms: 1,
+      bathrooms: 1,
+      parking: false,
+      furnished: false,
+      address: '',
+      offer: false,
+      regularPrice: 0,
+      discountedPrice: 0,
+      images: {},
+      latitude: 0,
+      longitude: 0,
+    })
+  
+    const {
+      type,
+      name,
+      bedrooms,
+      bathrooms,
+      parking,
+      furnished,
+      address,
+      offer,
+      regularPrice,
+      discountedPrice,
+      images,
+      latitude,
+      longitude,
+    } = formData
+  
+    const auth = getAuth()
+    const navigate = useNavigate()
+    const isMounted = useRef(true)
+  
+    useEffect(() => {
+      if (isMounted) {
+        onAuthStateChanged(auth, (user) => {
+          if (user) {
+            setFormData({ ...formData, userRef: user.uid })
+          } else {
+            navigate('/sign-in')
+          }
+        })
+      }
+  
+      return () => {
+        isMounted.current = false
+      }
+    }, [isMounted])
+  
+    const onSubmit = async (e) => {
+      e.preventDefault()
+  
+      setLoading(true)
+  
+      if (discountedPrice >= regularPrice) {
+        setLoading(false)
+        toast.error('Discounted price needs to be less than regular price')
+        return
+      }
+  
+      if (images.length > 6) {
+        setLoading(false)
+        toast.error('Max 6 images')
+        return
+      }
+  
+      let geolocation = {}
+      let location
+  
+      if (geolocationEnabled) {
+        const response = await fetch(
+            `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyAvh_t4uqEo_FRMdysperXfmlarm4tYTCY`
+        )
+  
+        const data = await response.json()
+  
+        geolocation.lat = data.results[0]?.geometry.location.lat ?? 0
+        geolocation.lng = data.results[0]?.geometry.location.lng ?? 0
+  
+        location =
+          data.status === 'ZERO_RESULTS'
+            ? undefined
+            : data.results[0]?.formatted_address
+  
+        if (location === undefined || location.includes('undefined')) {
+          setLoading(false)
+          toast.error('Please enter a correct address')
+          return
         }
-      });
+      } else {
+        geolocation.lat = latitude
+        geolocation.lng = longitude
+      }
+  
+      const storeImage = async (image) => {
+        return new Promise((resolve, reject) => {
+          const storage = getStorage()
+          const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`
+  
+          const storageRef = ref(storage, 'images/' + fileName)
+  
+          const uploadTask = uploadBytesResumable(storageRef, image)
+  
+          uploadTask.on(
+            'state_changed',
+            (snapshot) => {
+              const progress =
+                (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+              console.log('Upload is ' + progress + '% done')
+              switch (snapshot.state) {
+                case 'paused':
+                  console.log('Upload is paused')
+                  break
+                case 'running':
+                  console.log('Upload is running')
+                  break
+                default:
+                  break
+              }
+            },
+            (error) => {
+              reject(error)
+            },
+            () => {
+              
+              getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                resolve(downloadURL)
+              })
+            }
+          )
+        })
+      }
+  
+      const imgUrls = await Promise.all(
+        [...images].map((image) => storeImage(image))
+      ).catch(() => {
+        setLoading(false)
+        toast.error('Images not uploaded')
+        return
+      })
+  
+      const formDataCopy = {
+        ...formData,
+        imgUrls,
+        geolocation,
+        timestamp: serverTimestamp(),
+      }
+  
+      formDataCopy.location = address
+      delete formDataCopy.images
+      delete formDataCopy.address
+      !formDataCopy.offer && delete formDataCopy.discountedPrice
+  
+      const docRef = await addDoc(collection(db, 'listings'), formDataCopy)
+      setLoading(false)
+      toast.success('Listing saved')
+      navigate(`/category/${formDataCopy.type}/${docRef.id}`)
     }
-
-    return () => {
-      isMounted.current = false;
-    };
-  }, [isMounted]);
-
-  if (loading) {
-    return <Spinner />;
-  }
-  const onSubmit = (e) => {
-    e.preventDefault();
-  };
-
-  const onMutate = (e) => {
-    let boolean = null;
-
-    if (e.target.value === "true") {
-      boolean = true;
-    }
-    if (e.target.value === "false") {
-      boolean = false;
-    }
-    if(e.target.files){
+  
+    const onMutate = (e) => {
+      let boolean = null
+  
+      if (e.target.value === 'true') {
+        boolean = true
+      }
+      if (e.target.value === 'false') {
+        boolean = false
+      }
+  
+      if (e.target.files) {
         setFormData((prevState) => ({
-            ...prevState,
-            images:e.target.files
+          ...prevState,
+          images: e.target.files,
         }))
-    }
-    if(!e.target.files){
+      }
+  
+      if (!e.target.files) {
         setFormData((prevState) => ({
-            ...prevState,
-            [e.target.id] : boolean ?? e.target.value
+          ...prevState,
+          [e.target.id]: boolean ?? e.target.value,
         }))
+      }
     }
-  };
+  
+    if (loading) {
+      return <Spinner />
+    }
+  
 
   return (
     <div className="profile">
