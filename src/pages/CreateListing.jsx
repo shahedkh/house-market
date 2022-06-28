d4383fc6cf5b500cc9e3691a6e05030175c3ecfc
shahedkh <alkhalailah.shahed@gmail.com>
2022-06-28 10:04:19 +0300

    start create listing

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,6 +1,77 @@
-import React from 'react'
+import { useState, useEffect, useRef } from 'react'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from 'firebase/storage'
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
+import { db } from '../firebase.config'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { v4 as uuidv4 } from 'uuid'
+import Spinner from '../components/Spinner'
 
 function CreateListing() {
+    const [geolocationEnabled , setGeolocationEnabled] = useState(true)
+    const [loading , setLoading] = useState(false)
+    const [formData , setFormData] = useState({
+        type: 'rent',
+        name: '',
+        bedrooms: 1,
+        bathrooms: 1,
+        parking: false,
+        furnished: false,
+        address: '',
+        offer: false,
+        regularPrice: 0,
+        discountedPrice: 0,
+        images: {},
+        latitude: 0,
+        longitude: 0,
+      })
+    
+      const {
+        type,
+        name,
+        bedrooms,
+        bathrooms,
+        parking,
+        furnished,
+        address,
+        offer,
+        regularPrice,
+        discountedPrice,
+        images,
+        latitude,
+        longitude,
+      } = formData
+
+      const auth = getAuth()
+      const navigate = useNavigate()
+      const isMounted = useRef(true)
+
+      useEffect(() =>{
+        if(isMounted){
+            onAuthStateChanged(auth , (user) => {
+                if(user){
+                    setFormData({...formData , userRef : user.uid})
+                }else{
+                    navigate('/sign-in')
+                }
+            })
+        }
+
+        return () => {
+            isMounted.current = false
+        }
+      },[isMounted])
+
+      if(loading){
+        return <Spinner/>
+      }
+
   return (
     <div>CreateListing</div>
   )
